1575dcdf875747b9bd206cde3124425612708828
Ivan Keynes <fajny.kod.pl@gmail.com>
2024-03-10 19:38:20 +0100

    Initial commit

---
+++ Kot.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _15_przeciazanie_metod
+{
+    internal class Kot
+    {
+        public void PokazInformacje(string imie)
+        {
+            Console.WriteLine($"imie: {imie}");
+        }
+
+        public void PokazInformacje(string imie, int wiek)
+        {
+            Console.WriteLine($"imie: {imie} \nwiek: {wiek}");
+        }
+
+        public void PokazInformacje(string imie, string kolorOczu)
+        {
+            Console.WriteLine($"imie: {imie} \nkolor oczu: {kolorOczu}");
+        }
+
+        public void PokazInformacje(string imie, string kolorOczu, string kolorSiersci)
+        {
+            Console.WriteLine($"imie: {imie} \nkolor oczu: {kolorOczu} \nkolor siersci: {kolorSiersci}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using _15_przeciazanie_metod;
+
+Kot mruczek = new Kot();
+
+mruczek.PokazInformacje("Mruczek");
+
+mruczek.PokazInformacje("Mruczek", 7);
+
+Kot  bella = new Kot();
+
+bella.PokazInformacje("Bella", "Zolty", "Czarny");
